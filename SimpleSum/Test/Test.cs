05779f05011d2c6c3e322dc319a9eda533f6880b
ramrajnkl <getramraj@outlook.com>
2021-09-08 07:36:46 +0530

    Simple Sum Init

---
+++ SimpleSum/Test/Test.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace Test
+{
+    [TestClass]
+    public class Test
+    {
+        [TestMethod]
+        public void Add()
+        {
+            var a = 100;
+            var b = 100;
+
+            int Result = a + b;
+
+            Assert.AreEqual(200, Result);
+                       
+        }
+
+        [TestMethod]
+        public void Sub()
+        {
+            var a = 100;
+            var b = 100;
+
+            int Result = a - b;
+
+            Assert.AreEqual(0, Result);
+
+        }
+
+        [TestMethod]
+        public void Mul()
+        {
+            var a = 100;
+            var b = 100;
+
+            int Result = a * b;
+
+            Assert.AreEqual(10000, Result);
+
+        }
+
+
+        [TestMethod]
+        public void Div()
+        {
+            var a = 100;
+            var b = 100;
+
+            int Result = a / b;
+
+            Assert.AreEqual(1, Result);
+
+        }
+        
+    }
+}
